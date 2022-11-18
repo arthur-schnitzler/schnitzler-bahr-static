6783f1d6e59b97d5f51a3205c8502f922410afcb
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2022-11-18 09:38:35 +0100

    returned to babslgams calendar-fix

--- make_calendar_data.py
+++ make_calendar_data.py
@@ -3,27 +3,28 @@ import os
 import json
 from acdh_tei_pyutils.tei import TeiReader
 from tqdm import tqdm
-files = sorted(glob.glob('./data/editions/L*.xml'))
-out_file = "./html/js-data/calendarData.js"
+files = sorted(glob.glob('./data/editions/*.xml'))
+out_file = "./html/calendarData.js"
 data = []
 for x in tqdm(files, total=len(files)):
     item = {}
     head, tail = os.path.split(x)
     doc = TeiReader(x)
-    item['name'] = doc.any_xpath('//tei:titleStmt/tei:title[@level="a"]/text()')[0]
+    item['name'] = doc.any_xpath('//tei:title[@level="a"]/text()')[0]
     try:
-        item['startDate'] = doc.any_xpath('//tei:titleStmt/tei:title[@type="iso-date"]/@when-iso')[0]
+        item['startDate'] = doc.any_xpath('//tei:title[@type="iso-date"]/text()')[0]
     except:
         continue
     try:
-        item['tageszaheler'] = doc.any_xpath('//tei:titleStmt/tei:title[@type="iso-date"]/@n')[0]
+        item['tageszaheler'] = doc.any_xpath('//tei:title[@type="iso-date"]/@n')[0]
+        item['id'] = tail.replace('.xml', '.html')
+        data.append(item)
     except:
-        item['tageszaheler'] = "01"
-    item['id'] = tail.replace('.xml', '.html')
-    data.append(item)
+        continue
 
 print(f"writing calendar data to {out_file}")
 with open(out_file, 'w',  encoding='utf8') as f:
     my_js_variable = f"var calendarData = {json.dumps(data, ensure_ascii=False)}"
     f.write(my_js_variable)
+
 
