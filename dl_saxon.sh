340d432bfa372d36349cd2bfef28305d828c2b8c
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-12-04 22:56:47 +0100

    saxon muss nicht mehr im workflow geladen werden

--- dl_saxon.sh
+++ dl_saxon.sh
@@ -1,2 +1,7 @@
-echo "downloading saxon"
-wget https://sourceforge.net/projects/saxon/files/Saxon-HE/9.9/SaxonHE9-9-1-7J.zip/download && unzip download -d saxon && rm -rf download
+if [ -d "saxon" ] && [ -f "saxon/saxon9he.jar" ]; then
+    echo "Saxon already exists (found saxon/saxon9he.jar), skipping download"
+else
+    echo "Saxon not found, downloading..."
+    wget https://sourceforge.net/projects/saxon/files/Saxon-HE/9.9/SaxonHE9-9-1-7J.zip/download && unzip -o download -d saxon && rm -rf download
+    echo "Saxon download complete"
+fi
